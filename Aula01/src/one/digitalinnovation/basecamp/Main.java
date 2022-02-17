feaf577edfc4be85cbbaf180428860077224172a
Gabriel <gabrielsiodoni@hotmail.com>
2022-02-16 23:02:57 -0300

    Variáveis, Tipos de Dados e Operadores Matemáticos em Java

---
+++ Aula01/src/one/digitalinnovation/basecamp/Main.java
@@ -0,0 +1,50 @@
+package one.digitalinnovation.basecamp;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int i;
+        //int i;
+        int I;
+        //int 1a;
+        int _1a;
+        int $aq;
+
+        i = 5;
+        I = 10;
+        _1a = 20;
+        $aq = 7;
+
+        final int j = 10;
+        //j = 15;
+        int asrn24678md;
+        //int asrn246 78md;
+        int asrn24678_md = 10;
+        //int asrn246%78_md = 10;
+
+        asrn24678md = 110;
+        asrn24678_md = 10;
+
+        int quantidadeProduto = 50;
+        //int QuantidadeProduto;
+        final int NUMERP_TENTATIVAS = 5;
+        //final int numero_tentativas = 5;
+        int QUANBTIDADE_OPCOES = 25;
+        //int qtdProd;
+
+        System.out.println(i);
+        System.out.println(I);
+        System.out.println(_1a);
+        System.out.println($aq);
+
+        System.out.println(j);
+        System.out.println(asrn24678md);
+        System.out.println(asrn24678_md);;
+
+        System.out.println(quantidadeProduto);
+        System.out.println(NUMERP_TENTATIVAS);
+        System.out.println(QUANBTIDADE_OPCOES);
+
+    }
+}
